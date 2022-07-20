005f2c47a1ba2544e83f8e335a7af0a9738db76c
Prem singh <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-07-20 18:44:14 +0530

    Add files via upload

---
+++ call_packages/call_operators.java
@@ -0,0 +1,20 @@
+package CALL_ALL_PACKAGES;
+import CONDITIONAL_PACK.cond;
+public class call_operators extends cond{
+  public static void main(String args[]) 
+  {
+	  call_operators obj=new call_operators ();
+	  obj.arthmetic_opr();//call the method
+	  System.out.println();//use for space
+	  obj.bitwise_opr();//call the method
+	  System.out.println();//use for space
+	  obj.assig_opr();//call the method
+	  System.out.println();//use for space
+	 obj.logical_opr();//call the method
+	 System.out.println();//use for space
+	 obj.tirnary_opr();//call the method
+	 System.out.println();//use for space
+	  obj.conditional_opr();//call the method
+  }
+   
+}
