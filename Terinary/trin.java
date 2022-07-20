b3ff06f9fdb00bd816fad967e641106f695c2ed4
Prem singh <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-07-20 18:27:55 +0530

    Add files via upload

---
+++ Terinary/trin.java
@@ -0,0 +1,12 @@
+package TERINARY_PACK;
+import LOGICAL_PACK.log;//use packages 
+public class trin extends log{//using inhertaince 
+	int max;//declared variable
+	public void tirnary_opr() {//create method
+		System.out.println("THIS IS FOR TERINARY OPERATION");
+		aceept();   //call aceept method
+		max = (a > b) ? a : b;
+	    System.out.println("Maximum is = " + max);
+	}
+
+}
