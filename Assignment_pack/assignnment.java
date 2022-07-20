af923298b5bdaea78516b0830f38932804387849
Prem singh <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-07-20 18:25:29 +0530

    Add files via upload

---
+++ Assignment_pack/assignnment.java
@@ -0,0 +1,17 @@
+package ASSIGNMENT_PACK;
+import BITWISE_PACK.Bit;//using packages
+public class assignnment extends Bit {//use inhertaince
+	public void assig_opr() {//create method
+		System.out.println("THIS IS FOR ASSIGNMENT OPERATION");
+		aceept();
+   //aasignment operation....
+		a=a+b;
+        b=b+a;
+   
+	System.out.println( "New values of a and b");
+	System.out.println( "a="+a);
+	System.out.println( "b="+b);
+	
+	}
+
+}
