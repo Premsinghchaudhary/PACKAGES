c6d2689c9302424b75415a6b45e68b20e1399837
Prem singh <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-07-20 18:33:48 +0530

    Add files via upload

---
+++ Conditional_pack/cond.java
@@ -0,0 +1,15 @@
+package CONDITIONAL_PACK;
+import TERINARY_PACK.trin;//use packages
+public class cond extends trin{//using inhertaince
+public void conditional_opr() {//create method
+	System.out.println("THIS IS FOR CINDITIONAL OPERATION");
+	aceept();
+	//use statement
+	if( a>b) {
+		System.out.println("a is greater:"+a);
+	}
+	else {
+		System.out.println("b is greater:"+b);
+	}
+}
+}
