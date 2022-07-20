28cc94539c86064521dcc8caf3edd98433b182bf
Prem singh <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-07-20 18:23:15 +0530

    Add files via upload

---
+++ Arthemtic_pack/arthmetic.java
@@ -0,0 +1,19 @@
+package ARTHEMTIC_PACK;
+
+import Acpt_no.input_from_user;//use packages
+
+public class arthmetic extends input_from_user {//using inhertiance properties
+	
+			
+	public  void arthmetic_opr() {//create a method
+		System.out.println("THIS IS FOR ARTHEMETIC OPERATION");
+		aceept();
+		// calculation...........
+	System.out.println( " The addition of two no is : "+(a+b)); //The sum(+) of a and b
+	System.out.println( " The substraction of two no is : "+(a-b));//the sub (-) of a and b
+	System.out.println( " The multiplication of two no is : "+(a*b));//the mul (*) of a and b
+	System.out.println( " The division of two no is : "+(a/b));//the division (/) of a and b
+	System.out.println( " The modulas  is : "+(a%b));//the modulas (%) of a and b
+	}
+	
+}
